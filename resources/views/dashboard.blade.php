993e50513a8a3f2a815672a17358817998e43fd7
Adi Fauzan Akbar <adiaz@engineer.com>
2022-12-06 14:41:14 +0700

    Logika IF

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,36 @@
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="{{ __('former.js') }}"></script>
+</head>
+<body>
+    
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+        <div class="d-flex justify-content-around">
 
+
+        </div>
+        <form id="form" onsubmit="return false">
+            <div class="form-group d-flex justify-content-around">
+                <div class="form-group col-6">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Nilai') }}
+                    </h2>
+                    <input type="text" class="form-control mt-1" id="inputNilai" placeholder="Input Nilai">
+                </div>
+                <div class="form-group col-6">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Keterangan') }}
+                    </h2>    
+                    <h1 id="hasil"></h1>
+                </div>
+            </div>
+            <button type="submit" class="btn border ml-3 -mt-4" onclick="cek()">Submit</button>
+        </form>
+        
+    </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -15,3 +41,4 @@
         </div>
     </div>
 </x-app-layout>
+</body>
